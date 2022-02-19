14cb6900d43614e8d907a0063526a98ec390de32
Max <addergtx@gmail.com>
2022-02-19 20:31:12 +0300

    Реализовал рандомное создание фигур и запись их в массив фигур.

--- Figures/Program.cs
+++ Figures/Program.cs
@@ -242,55 +242,158 @@ namespace Figures
 
     class Program
     {
+        static private Figure GetFigure(int numberFigure, int numberTypeFigure)
+        {
+            switch (numberTypeFigure)
+            {
+                case (int)TypeFigure.Square:
+                    return new Square($"Квадрат №{numberFigure}", 10);
+                case (int)TypeFigure.Rectangle:
+                    return new Rectangle($"Прямоугольник №{numberFigure}", 10, 20);
+                case (int)TypeFigure.Triangle:
+                    return new Triangle($"Треугольник №{numberFigure}", 15, 15, 15);
+                case (int)TypeFigure.Circle:
+                    return new Circle($"Круг №{numberFigure}", 25);
+                case (int)TypeFigure.Cube:
+                    return new Cube($"Куб №{numberFigure}", 10);
+                case (int)TypeFigure.Ball:
+                    return new Ball($"Шар №{numberFigure}", 20);
+                default:
+                    throw new Exception("Тип фигуры не найден");
+            }
+        }
+
+        static public Figure[] GetArrayFigure(int number)
+        {
+            Random rnd = new Random();
+            Figure[] figures = new Figure[number];
+
+            var max = Enum.GetNames(typeof(TypeFigure)).Length;
+
+            for (int i = 0; i < figures.Length; i++)
+            {
+                var indexTypeFigure = rnd.Next(0, max - 1);
+                var numberFigure = i + 1;
+
+                figures[i] = GetFigure(numberFigure, indexTypeFigure);
+            }
+
+            return figures;
+        }
+
         static void Main(string[] args)
         {
+            int number = 0;
+
+            //while (number == 0)
+            //{
+            //    try
+            //    {
+            //        Console.Write("Задайте количество фигур: ");
+
+            //        string str = Console.ReadLine();
+            //        number = Convert.ToInt32(str);
+
+            //        Console.Write("Количество фигур равно {0}", number);
+            //        break;
+            //    }
+            //    catch (FormatException e)
+            //    {
+            //        Console.WriteLine("Input string is not a sequence of digits.");
+            //    }
+            //    catch (OverflowException e)
+            //    {
+            //        Console.WriteLine("The number cannot fit in int.");
+            //    }
+            //}
+
+            //number = 0;
+            //string figure;
+
+            //switch (number)
+            //{
+            //    case (int)TypeFigure.Square:
+            //        figure = "Квадрат";
+            //        break;
+            //    case (int)TypeFigure.Rectangle:
+            //        figure = "Прямоугольник";
+            //        break;
+            //    case (int)TypeFigure.Triangle:
+            //        figure = "Треугольник";
+            //        break;
+            //    case (int)TypeFigure.Circle:
+            //        figure = "Круг";
+            //        break;
+            //    case (int)TypeFigure.Cube:
+            //        figure = "Куб";
+            //        break;
+            //    case (int)TypeFigure.Ball:
+            //        figure = "Шар";
+            //        break;
+            //    default:
+            //        figure = "нет типа";
+            //        break;
+            //}
+
+            //Console.WriteLine(figure);
+
+            var figures = GetArrayFigure(2);
+
+            foreach (var figure in figures)
+            {
+                var name = figure.Name;
+                var type = figure.Type;
+
+                Console.WriteLine("Фигура типа {0}, с именем {1}", type, name);
+            }
+
             try
             {
-                var square1 = new Square("Квадрат №1", 10);
-                var rectangle1 = new Rectangle("Прямоугольник №1", 10, 20);
-                var triangle1 = new Triangle("Треугольник №1", 15, 15, 15);
-                var circle1 = new Circle("Круг №1", 25);
+                //var square1 = new Square("Квадрат №1", 10);
+                //var rectangle1 = new Rectangle("Прямоугольник №1", 10, 20);
+                //var triangle1 = new Triangle("Треугольник №1", 15, 15, 15);
+                //var circle1 = new Circle("Круг №1", 25);
 
-                var cube1 = new Cube("Куб №1", 10);
-                var ball1 = new Ball("Шар №1", 20);
+                //var cube1 = new Cube("Куб №1", 10);
+                //var ball1 = new Ball("Шар №1", 20);
 
-                Console.WriteLine("Фигура типа {0}, с именем {1}, имеет площадь равную {2} и перимерт равный {3}",
-                    square1.Type,
-                    square1.Name,
-                    square1.Space(),
-                    square1.Perimeter());
+                //Console.WriteLine("Фигура типа {0}, с именем {1}, имеет площадь равную {2} и перимерт равный {3}",
+                //    square1.Type,
+                //    square1.Name,
+                //    square1.Space(),
+                //    square1.Perimeter());
 
-                Console.WriteLine("Фигура типа {0}, с именем {1}, имеет площадь равную {2} и перимерт равный {3}",
-                    rectangle1.Type,
-                    rectangle1.Name,
-                    rectangle1.Space(),
-                    rectangle1.Perimeter());
+                //Console.WriteLine("Фигура типа {0}, с именем {1}, имеет площадь равную {2} и перимерт равный {3}",
+                //    rectangle1.Type,
+                //    rectangle1.Name,
+                //    rectangle1.Space(),
+                //    rectangle1.Perimeter());
 
-                Console.WriteLine("Фигура типа {0}, с именем {1}, имеет площадь равную {2} и перимерт равный {3}",
-                    triangle1.Type,
-                    triangle1.Name,
-                    triangle1.Space(),
-                    triangle1.Perimeter());
+                //Console.WriteLine("Фигура типа {0}, с именем {1}, имеет площадь равную {2} и перимерт равный {3}",
+                //    triangle1.Type,
+                //    triangle1.Name,
+                //    triangle1.Space(),
+                //    triangle1.Perimeter());
 
-                Console.WriteLine("Фигура типа {0}, с именем {1}, имеет площадь равную {2} и перимерт равный {3}",
-                    circle1.Type,
-                    circle1.Name,
-                    circle1.Space(),
-                    circle1.Perimeter());
+                //Console.WriteLine("Фигура типа {0}, с именем {1}, имеет площадь равную {2} и перимерт равный {3}",
+                //    circle1.Type,
+                //    circle1.Name,
+                //    circle1.Space(),
+                //    circle1.Perimeter());
 
-                Console.WriteLine("Фигура типа {0}, с именем {1}, имеет площадь равную {2}, перимерт равный {3} и объем равный {4}",
-                    cube1.Type,
-                    cube1.Name,
-                    cube1.Space(),
-                    cube1.Perimeter(),
-                    cube1.Volume());
+                //Console.WriteLine("Фигура типа {0}, с именем {1}, имеет площадь равную {2}, перимерт равный {3} и объем равный {4}",
+                //    cube1.Type,
+                //    cube1.Name,
+                //    cube1.Space(),
+                //    cube1.Perimeter(),
+                //    cube1.Volume());
 
-                Console.WriteLine("Фигура типа {0}, с именем {1}, имеет площадь равную {2}, перимерт равный {3} и объем равный {4}",
-                    ball1.Type,
-                    ball1.Name,
-                    ball1.Space(),
-                    ball1.Perimeter(),
-                    ball1.Volume());
+                //Console.WriteLine("Фигура типа {0}, с именем {1}, имеет площадь равную {2}, перимерт равный {3} и объем равный {4}",
+                //    ball1.Type,
+                //    ball1.Name,
+                //    ball1.Space(),
+                //    ball1.Perimeter(),
+                //    ball1.Volume());
             }
             catch (Exception ex)
             {
